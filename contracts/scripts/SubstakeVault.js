229d48c2c53584359d659fb5e973051de5537345
Anubhav Kumar <anubhav11697@gmail.com>
2023-12-03 15:28:35 +0530

    L2: fixed SubstakeVault script

--- contracts/scripts/SubstakeVault.js
+++ contracts/scripts/SubstakeVault.js
@@ -1,15 +1,14 @@
 const {ethers, JsonRpcProvider} = require("ethers");
-
+require('dotenv').config();
 let fs= require('fs');
 const fsPromise = fs.promises;
 
-const ALCHEMY_RPC_URL = ""
-const privateKey = ""
+const scrollSepoliaRPC = process.env.SCROLL_RPC;
+const privateKey = process.env.PV_KEY
 
 const vaultProxyabipath = "../out/SubstakeVaultProxy.sol/SubstakeVaultProxy.json";
-const vaultProxyAddress = "";
-
-const scrollSepoliaRPC = ""
+const vaultProxyAddress = "0xC4374cC35CbB2a42B9C19495AD811C742dc9FAA9";
+const vaultImplementationAbiPath = "../out/SubstakeVault.sol/SubstakeVault.json";
 
 const provider = new JsonRpcProvider(scrollSepoliaRPC);
 const signer = new ethers.Wallet(privateKey, provider);
@@ -20,14 +19,14 @@ async function getAbi(path){
     return abi;
 }
 
-const main = async () =>{
-
+const main = async () => {
+    // upgradeImplementation("0x");
+    // initializeVault()
 }
 
-const upgradeImplementation = async () => {
+const upgradeImplementation = async (vaultImplementation) => {
     const PROXY_ABI = await getAbi(vaultProxyabipath);
-    const contract = new ethers.Contract(VaultProxyAddress, PROXY_ABI.abi, signer);
-    const vaultImplementation = "";
+    const contract = new ethers.Contract(vaultProxyAddress, PROXY_ABI.abi, signer);
     console.log("Updating implementaion.........................");
     let tx = await contract.upgradeImplementation(vaultImplementation)
     await tx.wait()
@@ -41,12 +40,12 @@ const upgradeImplementation = async () => {
 }
 
 const initializeVault = async () => {
-    const IMPLEMENTATION_ABI = await getAbi(vaultImplementationabipath);
-    const contract = new ethers.Contract(VaultProxyAddress, IMPLEMENTATION_ABI.abi, signer);
+    const IMPLEMENTATION_ABI = await getAbi(vaultImplementationAbiPath);
+    const contract = new ethers.Contract(vaultProxyAddress, IMPLEMENTATION_ABI.abi, signer);
     const admin = "0x55d9a0d367866a102eD85EA76CE46B11E62b3E88";
-    const L2config ="0x7a5483542b602e130a05Db23a7E2AeC59b2F08C6";
+    const L2config ="0x7BCaa65E6cAceF4FB7F2852488829bd92090667a";
     console.log("Initializing Vault.................................");
-    let tx = await contract.initialize(admin,L2config);
+    let tx = await contract.initialize(admin, L2config);
     await tx.wait()
     .then(() => {
         console.log("vault Initialized!");
@@ -55,12 +54,4 @@ const initializeVault = async () => {
         console.log("Failed to initialize vault!");
         console.log(error);
     })
-}
-const deposit = async () => {
-    const VAULT_ABI =await getAbi(Vaultabipath);
-    const contract = new ethers.Contract();
-  
-    console.log("calling deposit(uint256, address");
-
-
 }
