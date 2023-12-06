ab8baef6e18069908cc11c129d6c8ca4af450df9
Anubhav Kumar <anubhav11697@gmail.com>
2023-12-06 10:51:45 +0530

    Vault major testin [In Progress]

--- contracts/scripts/VaultActions.js
+++ contracts/scripts/VaultActions.js
@@ -20,22 +20,29 @@ async function getAbi(path) {
 
 const main = async () => {
     //_deposit();
-    stakeDispatch();
+    // stakeDispatch();
+    // unstakeDispatch();
+    userDeposit();
 }
 
-const _deposit = async () => {
+const userDeposit = async () => {
     const IMPLEMENTATION_ABI = await getAbi(vaultImplementationAbiPath);
     const contract = new ethers.Contract(vaultProxyAddress, IMPLEMENTATION_ABI.abi, signer);
-    let tx = await contract.deposit()
-    await tx.wait()
-        .then(() => {
-            console.log("transaction successful");
-        })
-        .catch((error) => {
-            console.log("transaction failed.");
-            console.log(error);
-        })
+    const ethAmount = ethers.parseEther("0.001");
+    const receiver = "0x55d9a0d367866a102eD85EA76CE46B11E62b3E88";
     console.log("calling deposit(uint256, address)");
+    try {
+        let tx = await contract.deposit(ethAmount, receiver, { value: ethAmount, gasLimit: 1000000 });
+        let receipt = await tx.wait();
+        console.log("receipt : ", receipt);
+        const shares = receipt.events[0].args.shares;
+        const batchId = receipt.events[0].args.batchId;
+        
+        console.log("Deposit successful. Shares:", shares.toString(), "Stake Batch ID:", batchId.toString());
+    } catch (error) {
+        console.log("Deposit failed.");
+        console.log(error);
+    }
 }
 
 const stakeDispatch = async () => {
@@ -45,7 +52,7 @@ const stakeDispatch = async () => {
     console.log("calling dispatchStakeBatch()........");
     let tx = await contract.dispatchStakeBatch({
         value: ethAmount,
-        gasLimit: 1000000
+        gasLimit: 450000
     });
     await tx.wait()
         .then(() => {
@@ -56,6 +63,23 @@ const stakeDispatch = async () => {
             console.log(error);
         })
 
+}
+
+const unstakeDispatch = async () => {
+    const IMPLEMENTATION_ABI = await getAbi(vaultImplementationAbiPath);
+    const contract = new ethers.Contract(vaultProxyAddress, IMPLEMENTATION_ABI.abi, signer);
+    console.log("calling dispatchUnstakeBatch()........");
+    let tx = await contract.dispatchUnstakeBatch({
+        gasLimit: 400000
+    });
+    await tx.wait()
+        .then(() => {
+            console.log("Call Successful!");
+        })
+        .catch((error) => {
+            console.log("Failed to call!");
+            console.log(error);
+        })
 }
 
 main();
