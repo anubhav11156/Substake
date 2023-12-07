2756293bc924d0ce53a798b21ae41f035b256bb7
Hemant <hrsmart2150@gmail.com>
2023-12-07 16:47:36 +0530

    added subTokenBalance in navbar

--- frontend/components/MobileNavLinks.tsx
+++ frontend/components/MobileNavLinks.tsx
@@ -1,12 +1,18 @@
 import { ConnectKitButton } from "connectkit";
 import { Menu } from "lucide-react";
+import Image from "next/image";
+import { useAccount } from "wagmi";
 
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { cn } from "@/lib/utils";
 import NavLinks from "./NavLinks";
-import { Button } from "./ui/button";
+import { Button, buttonVariants } from "./ui/button";
 
-const MobileNavLinks: React.FC = () => {
+const MobileNavLinks: React.FC<{ subTokenBalance: string }> = ({
+  subTokenBalance,
+}) => {
+  const { address, isConnecting, isDisconnected, isConnected } = useAccount();
+
   return (
     <Sheet>
       <SheetTrigger>
@@ -16,30 +22,53 @@ const MobileNavLinks: React.FC = () => {
         <div className="flex flex-col gap-10 w-full items-center pt-10">
           <NavLinks className="flex flex-col" />
 
-          <ConnectKitButton.Custom>
-            {({
-              isConnected,
-              isConnecting,
-              show,
-              hide,
-              address,
-              ensName,
-              chain,
-            }) => {
-              return (
-                <Button
-                  onClick={show}
-                  className={cn(
-                    "rounded-xl font-normal uppercase transition-all bg-[#9b923b] hover:bg-[#a99f44] text-white/90 ring-offset-[#fadfb5] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-mainBg focus-visible:ring-offset-2"
-                  )}
-                >
-                  {isConnected
-                    ? `${address?.slice(0, 5)}...${address?.slice(-5)}`
-                    : "connect wallet"}
-                </Button>
-              );
-            }}
-          </ConnectKitButton.Custom>
+          <div className="flex flex-col items-center gap-2">
+            {isConnected && (
+              <div
+                className={cn(
+                  buttonVariants(),
+                  "rounded-xl font-thin uppercase transition-all w-full bg-[#9b923b] hover:bg-transparent text-white/90 ring-offset-[#fadfb5] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-mainBg focus-visible:ring-offset-2 flex items-center gap-2 truncate border border-mainBg bg-transparent"
+                )}
+              >
+                <div className="p-2 bg-mainBg rounded-full">
+                  <Image
+                    src="/logo_white.svg"
+                    width={15}
+                    height={15}
+                    alt="eth"
+                  />
+                </div>
+                <span className="truncate text-black">
+                  {!subTokenBalance ? "0.0000" : subTokenBalance}
+                </span>
+              </div>
+            )}
+
+            <ConnectKitButton.Custom>
+              {({
+                isConnected,
+                isConnecting,
+                show,
+                hide,
+                address,
+                ensName,
+                chain,
+              }) => {
+                return (
+                  <Button
+                    onClick={show}
+                    className={cn(
+                      "rounded-xl font-thin uppercase transition-all bg-[#9b923b] hover:bg-[#a99f44] text-white/90 ring-offset-[#fadfb5] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-mainBg focus-visible:ring-offset-2"
+                    )}
+                  >
+                    {isConnected
+                      ? `${address?.slice(0, 5)}...${address?.slice(-5)}`
+                      : "connect wallet"}
+                  </Button>
+                );
+              }}
+            </ConnectKitButton.Custom>
+          </div>
         </div>
       </SheetContent>
     </Sheet>
--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,16 +1,16 @@
 import { ConnectKitButton } from "connectkit";
 import Link from "next/link";
 
-import { Button } from "@/components/ui/button";
+import { VAULT_ABI } from "@/abi/abi";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { config, getAbi } from "@/configData";
 import { cn } from "@/lib/utils";
+import { JsonRpcProvider, N, ethers } from "ethers";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useAccount, useBalance } from "wagmi";
 import MobileNavLinks from "./MobileNavLinks";
 import NavLinks from "./NavLinks";
-import { N, ethers, JsonRpcProvider } from "ethers";
-import { useState, useEffect } from "react";
-import { VAULT_ABI } from "@/abi/abi";
-import { config, getAbi } from "@/configData";
-import { useAccount, useBalance } from "wagmi";
 
 interface NavbarProps {
   isNavLink?: boolean;
@@ -21,32 +21,34 @@ const Navbar: React.FC<NavbarProps> = ({
   isNavLink = true,
   isConnectWallet = true,
 }) => {
-
   const [subTokenBalance, setSubTokenBalance] = useState("");
-  const { address, isConnecting, isDisconnected } = useAccount();
+  const { address, isConnecting, isDisconnected, isConnected } = useAccount();
 
   const vaultProxyAddress = config.substake.l2.vaultProxy;
 
-  useEffect(()=>{
+  useEffect(() => {
     getUserSubTokenBalance();
-  },[subTokenBalance, address]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [subTokenBalance, address]);
 
   const getUserSubTokenBalance = async () => {
-    const jsonProvider = new JsonRpcProvider(process.env.NEXT_PUBLIC_SCROLL_RPC!);
-    const contract = new ethers.Contract(vaultProxyAddress,
+    const jsonProvider = new JsonRpcProvider(
+      process.env.NEXT_PUBLIC_SCROLL_RPC!
+    );
+    const contract = new ethers.Contract(
+      vaultProxyAddress,
       VAULT_ABI.abi,
       jsonProvider
     );
     try {
-      await contract.balanceOf(address)
-        .then((response) => {
-          let subBalance = (Number(response) / 10 ** 18).toFixed(6);
-          setSubTokenBalance(subBalance);
-        })
+      await contract.balanceOf(address).then((response) => {
+        let subBalance = (Number(response) / 10 ** 18).toFixed(6);
+        setSubTokenBalance(subBalance);
+      });
     } catch (error) {
-      console.log("Failed to fetch SUB balance ",error);
+      console.log("Failed to fetch SUB balance ", error);
     }
-  }
+  };
 
   return (
     <header className="w-full border-b border-mainBg transition-all">
@@ -73,37 +75,55 @@ const Navbar: React.FC<NavbarProps> = ({
           )}
         </div>
 
-        <ConnectKitButton.Custom>
-          {({
-            isConnected,
-            isConnecting,
-            show,
-            hide,
-            address,
-            ensName,
-            chain,
-          }) => {
-            return (
-              <Button
-                onClick={show}
-                className={cn(
-                  "rounded-xl font-medium uppercase transition-all w-36 bg-[#9b923b] hover:bg-[#a99f44] text-white/90 ring-offset-[#fadfb5] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-mainBg focus-visible:ring-offset-2",
-                  {
-                    "hidden sm:flex": !isConnectWallet,
-                  }
-                )}
-              >
-                {isConnected
-                  ? `${address?.slice(0, 5)}...${address?.slice(-5)}`
-                  : "connect"}
-              </Button>
-            );
-          }}
-        </ConnectKitButton.Custom>
+        <div className="flex items-center gap-2">
+          {isConnected && (
+            <div
+              className={cn(
+                buttonVariants(),
+                "rounded-xl font-medium uppercase transition-all w-full bg-[#9b923b] hover:bg-transparent text-white/90 ring-offset-[#fadfb5] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-mainBg focus-visible:ring-offset-2 items-center gap-2 truncate border border-mainBg bg-transparent hidden sm:flex"
+              )}
+            >
+              <div className="p-2 bg-mainBg rounded-full">
+                <Image src="/logo_white.svg" width={15} height={15} alt="eth" />
+              </div>
+              <span className="truncate text-black">
+                {!subTokenBalance ? "0.0000" : subTokenBalance}
+              </span>
+            </div>
+          )}
+
+          <ConnectKitButton.Custom>
+            {({
+              isConnected,
+              isConnecting,
+              show,
+              hide,
+              address,
+              ensName,
+              chain,
+            }) => {
+              return (
+                <Button
+                  onClick={show}
+                  className={cn(
+                    "rounded-xl font-medium uppercase transition-all w-36 bg-[#9b923b] hover:bg-[#a99f44] text-white/90 ring-offset-[#fadfb5] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-mainBg focus-visible:ring-offset-2",
+                    {
+                      "hidden sm:flex": !isConnectWallet,
+                    }
+                  )}
+                >
+                  {isConnected
+                    ? `${address?.slice(0, 5)}...${address?.slice(-5)}`
+                    : "connect"}
+                </Button>
+              );
+            }}
+          </ConnectKitButton.Custom>
+        </div>
 
         {isNavLink && (
           <div className="sm:hidden flex items-center gap-4">
-            <MobileNavLinks />
+            <MobileNavLinks subTokenBalance={subTokenBalance} />
           </div>
         )}
       </div>
