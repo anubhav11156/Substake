ab268aad71b7d7b1499e714cd9e893c2fe0c348d
Hemant <hrsmart2150@gmail.com>
2023-12-09 20:33:00 +0530

    removed unused imports

--- frontend/pages/api/getStakesByBatchId.ts
+++ frontend/pages/api/getStakesByBatchId.ts
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import { AddUserUntakesType } from "@/interfaces/UserStakes";
 import prismadb from "@/lib/prismadb";
 
 export default async function handler(
